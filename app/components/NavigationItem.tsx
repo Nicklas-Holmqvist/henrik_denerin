ad4a1910c3054979edbd4736ac279e6cd05f24df
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2023-07-04 19:43:35 +0200

    refactor: refactor and cleaned components

--- app/components/NavigationItem.tsx
+++ app/components/NavigationItem.tsx
@@ -1,33 +1,91 @@
 'use client';
 
-import React, { useState } from 'react';
+import Link from 'next/link';
+import React, { useEffect } from 'react';
+import { usePathname } from 'next/navigation';
 
-import MenuItem from './MenuItem';
+import { MenuItem } from '@/types/menuItems';
 
-interface NavigationItemProps {
-  menuItems: {
-    text: string;
-    path: string;
-    categories: {
-      tagtitle: string;
-    }[];
-  }[];
+interface navigationItemProps {
+  dropdown: string;
+  setDropdown: (name: string) => void;
+  menuItems: MenuItem;
 }
 
-const NavigationItem: React.FC<NavigationItemProps> = ({ menuItems }) => {
-  const [activeDropdown, setActiveDropdown] = useState<string>('');
+const NavigationItem: React.FC<navigationItemProps> = ({
+  dropdown,
+  setDropdown,
+  menuItems,
+}) => {
+  const pathname = usePathname();
+  const includesPathname = pathname.includes(menuItems.text);
+  const activeDropdown = dropdown === menuItems.text;
+
+  const subRef = React.useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleOutsideClick(event: any) {
+      if (subRef.current && !subRef.current.contains(event.target)) {
+        setDropdown('');
+      }
+    }
+    document.addEventListener('click', handleOutsideClick);
+    return () => document.removeEventListener('click', handleOutsideClick);
+  }, [setDropdown, subRef]);
+
   return (
-    <ul className="flex flex-column">
-      {menuItems.map((menuItem, index) => (
-        <li key={index} className="px-2 relative">
-          <MenuItem
-            menuItems={menuItem}
-            dropdown={activeDropdown}
-            setDropdown={(name: string) => setActiveDropdown(name)}
-          />
-        </li>
-      ))}
-    </ul>
+    <div ref={subRef}>
+      {menuItems.text !== 'works' ? (
+        <Link
+          onMouseEnter={() => {
+            setDropdown(menuItems.text);
+          }}
+          className={
+            includesPathname
+              ? 'border-bottom font-medium text-lg pb-0.5'
+              : 'border-hover font-medium text-lg pb-0.5'
+          }
+          href={menuItems.path}>
+          {menuItems.text}
+        </Link>
+      ) : (
+        <p
+          className={
+            includesPathname
+              ? 'border-bottom font-medium text-lg'
+              : 'pointer-cursor font-medium border-hover text-lg'
+          }
+          onClick={() => setDropdown(menuItems.text)}
+          onMouseEnter={() => setDropdown(menuItems.text)}>
+          {menuItems.text}
+        </p>
+      )}
+      {activeDropdown ? (
+        <ul className="flex-row absolute w-44 mt-2">
+          {menuItems.categories.length !== 0
+            ? menuItems.categories.map((category, index) => (
+                <li key={index} className="font-medium">
+                  {category.tagtitle === 'all' ? (
+                    <div className="border-t border-b mt-1 pb-1 border-darkblue">
+                      <Link
+                        href={`/works/${category.tagtitle}`}
+                        onClick={() => setDropdown('')}>
+                        {category.tagtitle} works [chronological]
+                      </Link>
+                    </div>
+                  ) : (
+                    <Link
+                      href={`/works/${category.tagtitle}`}
+                      onClick={() => setDropdown('')}>
+                      {category.tagtitle}
+                    </Link>
+                  )}
+                </li>
+              ))
+            : null}
+        </ul>
+      ) : null}
+    </div>
   );
 };
 
